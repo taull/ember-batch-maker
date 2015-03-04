3eb276428fe0387268c31581336cc2eda2767fc2
Taylor Aull <aull.taylor@gmail.com>
2015-03-04 14:51:14 -0500

    More form changes

--- app/routes/new.js
+++ app/routes/new.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-    createPost: function(newTitle, newName, newAuthor, newRecipeType, newPrepTime, newCookTime, newCookTemp, newCookTempUnits, newYieldNumber, newYieldName) {
+    createPost: function(newTitle, newName, newAuthor, newRecipeType, newPrepTime, newCookTime, newCookTemp, newCookTempUnits, newYieldNumber, newYieldUnits) {
       var self = this;
-      var info = {title: newTitle, name: newName, author: newAuthor, recipeType: newRecipeType, prepTime: newPrepTime, cookTime: newCookTime, cookTempUnits: newCookTempUnits, yieldNumber: newYieldNumber, yieldName: newYieldName};
+      var info = {title: newTitle, name: newName, author: newAuthor, recipeType: newRecipeType, prepTime: newPrepTime, cookTime: newCookTime, cookTempUnits: newCookTempUnits, yieldNumber: newYieldNumber, yieldName: newYieldUnits};
       Ember.$.ajax("https://api.parse.com/1/classes/Post/", {
         type: "POST",
         data: JSON.stringify(info)
